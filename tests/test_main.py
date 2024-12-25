a3d7f567cc805f807ebe5aaa3c7c0ae899a4ebce
sorachang <65143446+sorachang@users.noreply.github.com>
2024-12-25 20:12:07 +0800

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -14,13 +14,13 @@ def test_b():
     with pytest.raises((TypeError,ValueError)) as exp:
         b('a')
     assert "rror" in str(exp.value)
-    
+'''    
 def test_query(db_connection):
     # 使用db_connection進行測試
     client=socket.socket()
     client.connect(("172.16.1.113",8888))
     assert client.send(b"")==0
-
+'''
 @pytest.mark.parametrize("test_input, expected", [(1,1), (2,2), (3,3), (None,5)])
 def test_classapple(test_input,expected):
     if test_input is None:
